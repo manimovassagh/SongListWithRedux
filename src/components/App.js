47172972696fe2fe27125f14767fedca93b31a2f
Mani Movassagh <mani.mgh@gmail.com>
2021-03-10 15:19:04 +0100

    sematec ui addd

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <button class="ui secondary button">Okay</button>
-      <button class="ui button">Cancel</button>
-      <h2 class="ui icon header">
-        <i class="settings icon"></i>
-        <div class="content">
-          Account Settings
-          <div class="sub header">
-            Manage your account settings and set e-mail preferences.
-          </div>
-        </div>
-      </h2>
-      <div></div>
-    </div>
-  );
-};
-
+    return ( 
+        <h1>working</h1>
+     );
+}
+ 
 export default App;
