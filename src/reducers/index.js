7149b5e1388b552dde5871a0b910bb304fb9c7c8
Mani Movassagh <mani.mgh@gmail.com>
2021-03-10 15:47:31 +0100

    redux added

---
+++ src/reducers/index.js
@@ -0,0 +1,33 @@
+import {combineReducers} from 'redux';
+
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05",
+    },
+    {
+      title: "Macarena",
+      duration: "6:05",
+    },
+    {
+      title: "All start",
+      duration: "8:05",
+    },
+    {
+      title: "some songs",
+      duration: "11:25",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+export default combineReducers({
+    songs:songsReducer,
+    selectedSong:selectedSongReducer,
+});
