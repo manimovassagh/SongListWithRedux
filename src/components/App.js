41e973e73b312ac130d7f1c215a8a3082d587880
Mani Movassagh <mani.mgh@gmail.com>
2021-03-10 15:12:46 +0100

    4

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-    return ( <h1>Hi everything working fine! oder?</h1> );
-}
+  return (
+    <div>
+      <button class="ui secondary button">Okay</button>
+      <button class="ui button">Cancel</button>
+      <h2 class="ui icon header">
+        <i class="settings icon"></i>
+        <div class="content">
+          Account Settings
+          <div class="sub header">
+            Manage your account settings and set e-mail preferences.
+          </div>
+        </div>
+      </h2>
+      <div></div>
+    </div>
+  );
+};
 
 export default App;
